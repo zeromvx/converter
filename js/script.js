0f70749837196d61253584788d44c742f8fb09b0
zeromvx <zeromvx@gmail.com>
2020-07-16 09:44:12 +1000

    made codes refactoring

--- js/script.js
+++ js/script.js
@@ -1,45 +1,47 @@
-const curItem1 = document.querySelector('.converter__item-1');
-const curItem2 = document.querySelector('.converter__item-2');
-const curValue1 = document.querySelector('.converter__input-1');
-const curValue2 = document.querySelector('.converter__input-2');
+const currencyA = document.querySelector(".converter__item-1"); // select box of currency on top
+const currencyB = document.querySelector(".converter__item-2"); // select box of currency on bottom
+const countA = document.querySelector(".converter__input-1"); // input of currency`s count on top
+const countB = document.querySelector(".converter__input-2"); // input of currency`s count on bottom
 
-const changeBtn = document.querySelector('.change-btn');
-const rateBox = document.querySelector('.rate');
+const changeBtn = document.querySelector(".change-btn");
+const rateBox = document.querySelector(".rate"); // box for outputing data
 
 function calc() {
-    const curItemValue1 = curItem1.value;
-    const curItemValue2 = curItem2.value;
+	const valueA = currencyA.value;
+	const valueB = currencyB.value;
 
-    // Sending query to API
+	// Sending query to API
 
-    fetch(`https://api.exchangerate-api.com/v4/latest/${curItemValue1}`)
-        .then((res) => res.json())
-        .then((data) => {
-            const rate = data.rates[`${curItemValue2}`];
-            rateBox.textContent = `1 ${curItemValue1} = ${rate.toFixed(4)} ${curItemValue2}`;
-            curValue2.value = (curValue1.value * rate).toFixed(2);
-        });
+	fetch(`https://api.exchangerate-api.com/v4/latest/${valueA}`)
+		.then((res) => res.json())
+		.then((data) => {
+			const rate = data.rates[`${valueB}`];
+			rateBox.textContent = `1 ${valueA} = ${rate.toFixed(4)} ${valueB}`;
+			countB.value = (countA.value * rate).toFixed(2);
+		});
 }
 
 // When we clicking on buttons, triggered calc function that send query to api and update data
 
 function listeners() {
-    curItem1.addEventListener('change', calc);
-    curItem2.addEventListener('change', calc);
-    curValue1.addEventListener('input', calc);
-    curValue2.addEventListener('input', calc);
+	currencyA.addEventListener("change", calc);
+	currencyB.addEventListener("change", calc);
+	countA.addEventListener("input", calc);
+	countB.addEventListener("input", calc);
+	// variable for rotating switcher
+	let degIndex = 1;
 
-    let degIndex = 1;
+	changeBtn.addEventListener("click", () => {
+		[currencyA.value, currencyB.value] = [currencyB.value, currencyA.value];
 
-    changeBtn.addEventListener('click', () => {
-        [curItem1.value, curItem2.value] = [curItem2.value, curItem1.value];
-        changeBtn.style.transform = `rotate(${180 * degIndex}deg)`;
-        degIndex === 1 ? degIndex = 0 : degIndex++;
-        calc();
-    });
+		changeBtn.style.transform = `rotate(${180 * degIndex}deg)`;
+		degIndex === 1 ? (degIndex = 0) : degIndex++;
+
+		calc();
+	});
 }
 
-window.onload = () => {
-    listeners();
-    calc();
-};
+document.addEventListener("DOMContentLoaded", () => {
+	listeners();
+	calc();
+});
